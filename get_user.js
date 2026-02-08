571637281b439b9422bd10bc6b6bf89b040d3585
utsav <utsavmishraa005@gmail.com>
2026-02-08 13:55:18 +0530

    changes

--- get_user.js
+++
@@ -1,1 +0,0 @@
-const { PrismaClient } = require('@prisma/client'); const prisma = new PrismaClient(); prisma.user.findFirst().then(u => console.log(u ? u.email : 'no user')).finally(() => prisma.$disconnect())  
